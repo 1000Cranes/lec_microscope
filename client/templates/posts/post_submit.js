7084ef01f791799afeb792f3865c9350d7142d3f
lec <loura.coons@gmail.com>
2014-11-01 23:14:33 +0000

    Added method to to submit the post

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,6 +6,13 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+    
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      Router.go('postPage', {_id: result._id});  
+    });
 
     post._id = Posts.insert(post);
     Router.go('postPage', post);
